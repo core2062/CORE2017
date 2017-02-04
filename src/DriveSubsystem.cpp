763a501cb716740139c8fd146cf9640234408e24
Andrew Kempen <akemperfi@gmail.com>
2017-02-04 10:53:31 -0600

    More drive code

--- src/DriveSubsystem.cpp
+++ src/DriveSubsystem.cpp
@@ -1,11 +1,16 @@
 #include "DriveSubsystem.h"
 #include "Robot.h"
 
-DriveSubsystem::DriveSubsystem() : CORESubsystem("Drive Subsystem"), m_etherAValue("Ether A Value", 1),
-                                   m_etherBValue("Ether B Value", 1), m_etherQuickTurnValue("Ether Quick Turn Value", 2),
-                                   m_FRDrive(10), m_BRDrive(11), m_BLDrive(12), m_FLDrive(13),
+DriveSubsystem::DriveSubsystem() : CORESubsystem("Drive Subsystem"),
+								   m_etherAValue("Ether A Value", 1),
+                                   m_etherBValue("Ether B Value", 1),
+								   m_etherQuickTurnValue("Ether Quick Turn Value", 2),
+                                   m_FRDrive(FR_DRIVE_MOTOR_PORT), m_BRDrive(BR_DRIVE_MOTOR_PORT),
+								   m_BLDrive(BL_DRIVE_MOTOR_PORT), m_FLDrive(FL_DRIVE_MOTOR_PORT),
                                    m_drive(&m_BLDrive, &m_FLDrive, &m_BRDrive, &m_FRDrive, m_etherAValue.Get(),
-                                           m_etherBValue.Get(),m_etherQuickTurnValue.Get()), m_driveShifter() {
+                                           m_etherBValue.Get(),m_etherQuickTurnValue.Get()),
+                                   m_leftDriveShifter(LEFT_DRIVE_SHIFTER_HIGH_GEAR_PORT, LEFT_DRIVE_SHIFTER_LOW_GEAR_PORT),
+                                   m_rightDriveShifter(RIGHT_DRIVE_SHIFTER_HIGH_GEAR_PORT, RIGHT_DRIVE_SHIFTER_LOW_GEAR_PORT) {
 
 }
 
@@ -14,6 +19,7 @@ void DriveSubsystem::robotInit() {
 	m_BRDrive.setReversed(false);
 	m_BLDrive.setReversed(false);
 	m_FLDrive.setReversed(false);
+    Robot::driverJoystick->registerButton(COREJoystick::LEFT_BUTTON);
 }
 
 void DriveSubsystem::teleopInit() {
@@ -24,4 +30,29 @@ void DriveSubsystem::teleop() {
     double y = Robot::driverJoystick->getAxis(COREJoystick::LEFT_STICK_Y);
     double rot = Robot::driverJoystick->getAxis(COREJoystick::RIGHT_STICK_X);
     m_drive.cartesian(0, y, rot);
+    if(Robot::driverJoystick->getRisingEdge(COREJoystick::LEFT_BUTTON)) {
+        if(m_highGear) {
+            setLowGear();
+        } else {
+            setHighGear();
+        }
+    }
+}
+
+void DriveSubsystem::setHighGear(bool highGear) {
+    m_leftDriveShifter.Set(DoubleSolenoid::kForward);
+    m_rightDriveShifter.Set(DoubleSolenoid::kForward);
+}
+
+void DriveSubsystem::setLowGear(bool lowGear) {
+    m_leftDriveShifter.Set(DoubleSolenoid::kReverse);
+    m_rightDriveShifter.Set(DoubleSolenoid::kReverse);
+}
+
+bool DriveSubsystem::getHighGear() {
+    return m_highGear;
+}
+
+bool DriveSubsystem::getLowGear() {
+    return !m_highGear;
 }
--- src/DriveSubsystem.h
+++ src/DriveSubsystem.h
@@ -10,9 +10,14 @@ public:
     void robotInit() override;
     void teleopInit() override;
     void teleop() override;
+    void setHighGear(bool highGear = true);
+    void setLowGear(bool lowGear = true);
+    bool getHighGear();
+    bool getLowGear();
 private:
     COREConstant<double> m_etherAValue, m_etherBValue, m_etherQuickTurnValue;
     COREMotor m_FRDrive, m_BRDrive, m_BLDrive, m_FLDrive;
     COREEtherDrive m_drive;
-    DoubleSolenoid m_driveShifter;
+    DoubleSolenoid m_leftDriveShifter, m_rightDriveShifter;
+    bool m_highGear;
 };

34d97f3b64dd1c8c6e0bd1d2e8c32c7e350e2652
Joshua Spleas <JoshuaSpleas@gmail.com>
2017-03-06 17:48:05 -0600

    More Auto

--- src/Autonomous/DriveForwardAuton.cpp
+++ src/Autonomous/DriveForwardAuton.cpp
@@ -7,7 +7,8 @@ using namespace CORE;
 DriveForwardAuton::DriveForwardAuton(double speedInFraction, double distanceInFeet) :
 	COREAuton("Drive Forward", &m_moveForwardToLine, true) ,
 //	m_moveForwardToLine(new DriveWaypointAction(new Path({{{0,162},-100}, {{-12,162},-100}, {{-36,162},-100}/*, {{37,162},25}*/}),true)),
-	m_moveForwardToLine(new DriveWaypointAction(PathLoader::loadPath("driveForward.csv", -1.0), true)),
+//	m_moveForwardToLine(new DriveWaypointAction(PathLoader::loadPath("driveForward.csv", -1.0), true)),
+	m_moveForwardToLine(new DriveWaypointAction(PathLoader::loadPath("gearAuton_forward_boiler.csv", -1.0), true)),
 	m_spinInCircles(new GearFlapAction(ActuationType::OPEN)) {
 
 }
